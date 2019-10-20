671111db199b77ca1e0c863384ce82a27ebd4c6c
Carlos Pineda <carlos.r.pineda@gmail.com>
2019-10-20 21:35:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,19 +4,23 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  result = num1 - num2
+  p result
 end
 
 def division(num1, num2)
-
+  result = num1 / num2
+  p result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  p result
 end
 
 def modulo(num1, num2)
-
+  result = num1 + num2
+  p result
 end
 
 def square_root(num)
